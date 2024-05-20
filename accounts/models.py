882b3aeebb78e494cc34db64eac4d0893da65fbd
YunusBack <study.yunusgmail.com>
2024-05-20 23:15:51 +0300

    Removed django_home_6

--- accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.db import models
-
-class CustomUser(AbstractUser):
-    groups = models.ManyToManyField(Group, verbose_name=('groups'), blank=True, related_name='custom_user_set')
-    user_permissions = models.ManyToManyField(Permission, verbose_name=('user permissions'), blank=True, related_name='custom_user_set')
-
-    class Meta(AbstractUser.Meta):
-        swappable = 'AUTH_USER_MODEL'
